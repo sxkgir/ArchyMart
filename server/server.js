777e91b30a266461b4dc5caebc2c49f08d51c50e
sxkgir <143647406+sxkgir@users.noreply.github.com>
2025-07-08 14:55:15 -0400

    Set up Database

--- server/server.js
+++ server/server.js
@@ -2,11 +2,26 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const session = require("express-session");
-const connectDB = require("./db/ArchyMartDB")
+const { connectDB } = require("./db/ArchyMartDB")
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(
+    cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    })
+);
 
+connectDB()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Listening on port ${port}`);
+        });
+    })
+    .catch((err) => {
+        console.error("Database connection failed. Server not started.", err);
+    });
